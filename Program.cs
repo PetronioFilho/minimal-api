146d5c62526ca1d96c1e4c01204e3170d57fccca
PetronioFilho <camposfilho84@gmail.com>
2025-09-02 13:48:05 -0300

    Terceiro commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using minimal_api.Dominio.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -22,10 +24,3 @@ app.MapPost("/login", (LoginDTO loginDTO) =>
 });
 
 app.Run();
-
-
-public class LoginDTO
-{
-    public string Email { get; set; } = default!;
-    public string Senha { get; set; } = default!;
-}
